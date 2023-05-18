e88d9f97e6de673de6560b8789866b175b3e75d9
OLEKSANDR KARVAN <al.karvan@gmail.com>
2023-05-18 14:53:44 +0300

    JS lesson6 HW - Array methods (CallBack array.reduce and array.includes, push)

---
+++ Home Task JS/LESSON6/HW6_55.js
@@ -0,0 +1,67 @@
+let taskX = '--- TASK';
+const separator = '-------------------------';
+
+// TASK 6-55 - start
+
+// ----------------
+//      взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+// --написати пошук всіх об'єктів, в який в modules є sass
+// --написати пошук всіх об'єктів, в який в modules є docker
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+let searchValueInArrayModules = coursesArray.reduce((previousValue, currentValue) => {
+        if (currentValue.modules.includes('sass')) {
+            previousValue.moduleContainsSass.push(currentValue)
+        }
+        if (currentValue.modules.includes('docker') || currentValue.modules.includes('sass')) {
+            previousValue.moduleContainsDocker.push(currentValue)
+        }
+        return previousValue
+    },
+    // описує об"єкт {previousValue}
+    {moduleContainsDocker: [], moduleContainsSass: []})
+
+console.log(searchValueInArrayModules);
+// ----------------
+// document.write(separator);
+// console.log(separator);
+// TASK 6-55 - end
+
